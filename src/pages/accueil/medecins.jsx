5c54ae38c0688996790b9f50da15805d36a3077b
Thibault DEHU <Thibaultdehu2004@gmail.com>
2024-11-28 11:18:18 +0100

    2eme Commit

--- src/pages/accueil/medecins.jsx
+++ src/pages/accueil/medecins.jsx
@@ -1,19 +1,14 @@
-import { useState } from 'react'
 import { useLocation } from 'react-router-dom';
 import '../index.css'
-import Navbar from '/home/administrateur/vs/React/GSB Project/GSB/src/composants/Navbar.jsx';
-import { Outlet } from 'react-router-dom';
-
 
 function Medecins() {
 
     return (
         <>
         
-    <Navbar />
     <h1>Page des medecins</h1>
     </>
-    )
+    );
 }
 
 export default Medecins;
